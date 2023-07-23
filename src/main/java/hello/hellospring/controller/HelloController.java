a7e6b4824bc75dc8e5417c6023cbf0e95d3de6eb
HeeHeeg <qmffndoden@naver.com>
2023-07-23 19:46:56 +0900

    API 객체 반환 이해하기

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -22,9 +23,33 @@ public class HelloController {
         return "hello-templates";
     }
 
-    //API
+    //API - http에 정보를 그대로 보내주는 형식
     @GetMapping("hello-string")
+    @ResponseBody
     public String helloStrign(@RequestParam("name") String name) {
         return "helle" + name;
     }
+
+    //이런걸 API방식이라고 얘기한다. - JSON방식으로 나온다. 키-벨류형식으로 나온다.
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("hohoho") String hihi) {
+        Hello hello = new Hello(); // 객체 생성.
+        hello.setHahaha(hihi); // 파라미터로 넘어온 name을 넣어줌.
+        return hello; // 위에 하던것과 다른 점 -> 문자가 아닌 객체를 리턴해준다.
+    }
+
+    //객체 먼저 생성. static으로 하면 이 안에서 class를 만들 수 있음.
+    static class Hello { // HelloController.Hello로 쓸 수 있다.
+        public String hahaha;
+
+        public String getHahaha() {
+            return hahaha;
+        }
+
+        public void setHahaha(String hahaha) {
+            this.hahaha = hahaha;
+        }
+    }
+
 }
